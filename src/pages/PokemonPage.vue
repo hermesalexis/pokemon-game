8a331c106b3ee78a7f0e3ca1d7a217418171c7d7
hermesalexis <hermesagm@gmail.com>
2022-06-15 17:56:15 -0500

    first commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+  <PokemonPicture :pokemonId="4" :showPokemon="false" />
+  <PokemonOptions />
+
+  <!-- TODO: Opciones -->
+</template>
+
+<script>
+
+import PokemonPicture from "@/components/PokemonPicture.vue"
+import PokemonOptions from "@/components/PokemonOptions.vue"
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+console.log( getPokemonOptions() )
+
+export default {
+  components: { PokemonPicture, PokemonOptions }
+}
+</script>
